77a96f476e67078033cb222ed61dcddc961b6645
tcannon469 <thomascannon469@gmail.com>
2026-05-11 19:54:07 -0500

    formatting update

--- installP02.py
+++ installP02.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
-# ##############################################
-#          Install shelf button
+# Install shelf button
 # Execute it once to install and set the button on the Script Editor
 #   import sys
 #   sys.path.append("PATH/CannonThomasP2_MayaUtilityTool")
 #   import installP02
 #   installP02.install_Button()
 
-##############################################
 import os
 import maya.cmds as cmds
 
 TOOL_FOLDER = r"C:\Users\Mili\Downloads\CannonThomasP2_MayaUtilityTool"
 SCRIPT_FILE = os.path.join(TOOL_FOLDER, "CannonThomas_P02_code.py")
 ICON_FILE = os.path.join(TOOL_FOLDER,"icon.png")
-
 
 def install_Button():
     if not os.path.exists(SCRIPT_FILE):
@@ -49,15 +46,12 @@ CannonThomas_P02_code.show()
     if os.path.exists(ICON_FILE):
         kwargs["image1"] = ICON_FILE
 
-
     cmds.shelfButton(**kwargs)
     print("Maya Tool Installed.")
     print(ICON_FILE)
 
-
 def mel_eval(command):
     import maya.mel as mel
     return mel.eval(command)
-
 
 install_Button()
